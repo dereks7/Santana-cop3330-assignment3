73d176c8bceeae35408e23713573a05ca6ace3af
dereks7 <90410123+dereks7@users.noreply.github.com>
2021-10-11 16:30:27 -0400

    4:08

---
+++ src/test/java/Test_ex43.java
@@ -0,0 +1,15 @@
+import ex43.App;
+import org.junit.Test;
+
+public class Test_ex43
+{
+    @Test
+    public void ex43Test()
+    {
+        App test = new App();
+        String name = "Cooking";
+        String auth = "Josh";
+        char JS = 'N';
+        char CSS = 'N';
+    }
+}
